b1201f059dbad01f6879850cc35d98b181d1c1c9
Starup <billionaire@starup.(none)>
2013-07-20 01:32:57 -0400

    hw3 reconstruction

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
-var app = express();
-app.use(express.logger());
+
+// var app = express();
+var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  var fs = require('fs');
+  var buffer = fs.readFileSync('index.html');
+  response.send(buffer.toString("utf8"));
+  // response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
